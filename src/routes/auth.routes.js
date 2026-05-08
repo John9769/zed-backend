ba740721aa0ddc116c85a3af3ea63bdb54c5b1b0
John9769 <piyan6979@gmail.com>
2026-05-09 02:04:35 +0800

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { registerStudent, loginStudent, requestParentApproval } = require('../controllers/authController');
+const { registerStudent, loginStudent, requestParentApproval, addSubject } = require('../controllers/authController');
 const { authenticateStudent } = require('../middleware/auth');
 
 router.post('/register', registerStudent);
 router.post('/login', loginStudent);
 router.post('/request-approval', authenticateStudent, requestParentApproval);
+router.post('/add-subject', authenticateStudent, addSubject);
 
 module.exports = router;
